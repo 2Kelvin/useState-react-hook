b7aada36a138f952776607c5f2fbe5c2a7338092
2Kelvin <rocketmancodes02@gmail.com>
2024-02-16 10:59:50 +0300

    add all the texts

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import profilePic from './prof.jpg';
+import { FaCaretDown } from "react-icons/fa6";
 
 function App() {
   return (
@@ -9,10 +10,41 @@ function App() {
         <div className='imageWrapper'>
           <img src={profilePic} alt='profile' />
         </div>
+
+        <div className='followWrapper'>
+          <div className='stats'>
+            <div className='postsStatsDiv'>
+              <p>3,617</p>
+              <p>posts</p>
+            </div>
+
+            <div className='followersStatsDiv'>
+              <p>500</p>
+              <p>followers</p>
+            </div>
+
+            <div className='followingStatsDiv'>
+              <p>969</p>
+              <p>following</p>
+            </div>
+          </div>
+
+          <div className='buttonsDiv'>
+            <button className='followBtn'></button>
+            <button> <FaCaretDown /> </button>
+          </div>
+        </div>
       </div>
 
       {/* profile bio */}
-      <div className='profileBio'></div>
+      <div className='profileBio'>
+        <h3>Courtney Styles</h3>
+        <p className='career'>Photographer</p>
+        <p>🏆 2023 Best photographer of the year</p>
+        <p>Founder of <span className='blueLink'>@photosYellow</span></p>
+        <p>Inquiries: <span className='blueLink'>courtney@email.com</span></p>
+        <span className='blueLink'>www.stylesPhotography.com</span>
+      </div>
     </div>
   );
 }
