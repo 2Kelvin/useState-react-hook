803b3b9f6fe11dc0301f134529ebeb12e9d6c28d
2Kelvin <rocketmancodes02@gmail.com>
2024-02-16 10:22:57 +0300

    start proj: add main sections sketch

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import './App.css';
+import profilePic from './prof.jpg';
 
 function App() {
   return (
     <div className="App">
-      
+      {/* profile picture & following */}
+      <div className='picAndFollow'>
+        <div className='imageWrapper'>
+          <img src={profilePic} alt='profile' />
+        </div>
+      </div>
+
+      {/* profile bio */}
+      <div className='profileBio'></div>
     </div>
   );
 }
